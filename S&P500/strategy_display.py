badce718a1af46b1bb1e5a759a77b1a7e99bcbaa
Yi Chen <162146061+rebornlife0218@users.noreply.github.com>
2025-05-18 21:45:25 +0800

    Update strategy_display.py

--- S&P500/strategy_display.py
+++ S&P500/strategy_display.py
@@ -28,7 +28,7 @@ selected_strategy = st.selectbox("請選擇一個策略：", strategy_options)
 # 顯示對應的圖片與說明
 if selected_strategy:
     # 圖片檔案路徑
-    image_path = os.path.join(f"Strategy{strategy_options.index(selected_strategy) + 1}.jpg")
+    image_path = f"S&P500/Strategy{strategy_options.index(selected_strategy) + 1}.jpg"
     
     try:
         st.write("**策略說明**：")
