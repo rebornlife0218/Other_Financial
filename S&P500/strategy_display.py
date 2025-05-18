d33fe0964cad0e32f75625d59bddd78e31aea04f
Yi Chen <162146061+rebornlife0218@users.noreply.github.com>
2025-05-18 22:02:22 +0800

    Update strategy_display.py

--- S&P500/strategy_display.py
+++ S&P500/strategy_display.py
@@ -20,6 +20,9 @@ backtesting = {
     "Strategy 4": "Annual Return: 2.70%\nAnnual Volatility: 32.09%\nSharpe Ratio: 0.02\nMax Drawdown: -93.90%"
 }
 
+# SPY buy and hold 回測數據
+spy_backtesting = "Annual Return: 7.25%\nAnnual Volatility: 14.05%\nSharpe Ratio: 0.37\nMax Drawdown: -45.96%"
+
 # 策略選項
 strategy_options = ["Strategy 1", "Strategy 2", "Strategy 3", "Strategy 4"]
 selected_strategy = st.selectbox("請選擇一個策略：", strategy_options)
@@ -32,8 +35,16 @@ if selected_strategy:
     try:
         st.write("**策略說明**：")
         st.write(strategy_descriptions[selected_strategy])
-        st.write("**回測數據**：")
-        st.text(backtesting[selected_strategy])
+        
+       # 左右切分區塊
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("**策略回測數據**")
+            st.text(backtesting[selected_strategy])
+        with col2:
+            st.write("**SPY Buy and Hold 回測數據**")
+            st.text(spy_backtesting)
+            
         st.image(image_path, caption=selected_strategy, use_container_width=True)
         st.markdown("---")
         st.markdown("<div style='text-align: center; color: gray;'>由操盤人講堂第一組製作 2025.05</div>", unsafe_allow_html=True) 
